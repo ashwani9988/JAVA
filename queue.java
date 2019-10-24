7a2f1b25a7347d3d96d43d88db0011cf9dedc9ff
Anshul Gandotra <50234907+anshul1710@users.noreply.github.com>
2019-10-24 16:52:44 +0530

    update queue.java
    it's an easy way to do this.

--- queue.java
+++ queue.java
@@ -1,75 +1,38 @@
-import java.util.Stack; 
-  
-public class GFG { 
-   
-    static class Queue { 
-        Stack<Integer> stack1; 
-        Stack<Integer> stack2; 
-    } 
-  
-   
-    static void push(Stack<Integer> top_ref, int new_data) 
-    { 
-     
-        top_ref.push(new_data); 
-    } 
-  
-   
-    static int pop(Stack<Integer> top_ref) 
-    { 
-       
-        if (top_ref.isEmpty()) { 
-            System.out.println("Stack Underflow"); 
-            System.exit(0); 
-        } 
-  
-       
-        return top_ref.pop(); 
-    } 
-  
-   
-    static void enQueue(Queue q, int x) 
-    { 
-        push(q.stack1, x); 
-    } 
-  
-   
-    static int deQueue(Queue q) 
-    { 
-        int x; 
-  
-       
-        if (q.stack1.isEmpty() && q.stack2.isEmpty()) { 
-            System.out.println("Q is empty"); 
-            System.exit(0); 
-        } 
-  
-        
-      
-        if (q.stack2.isEmpty()) { 
-            while (!q.stack1.isEmpty()) { 
-                x = pop(q.stack1); 
-                push(q.stack2, x); 
-            } 
-        } 
-        x = pop(q.stack2); 
-        return x; 
-    } 
-  
-   
-    public static void main(String args[]) 
-    { 
-       
-        Queue q = new Queue(); 
-        q.stack1 = new Stack<>(); 
-        q.stack2 = new Stack<>(); 
-        enQueue(q, 1); 
-        enQueue(q, 2); 
-        enQueue(q, 3); 
-  
-       
-        System.out.print(deQueue(q) + " "); 
-        System.out.print(deQueue(q) + " "); 
-        System.out.println(deQueue(q) + " "); 
-    } 
-} 
+package Queue;
+
+import java.util.Stack;
+
+public class QueueWithStack {
+Stack<Integer> s1=new Stack<Integer>();
+Stack<Integer> s2=new Stack<Integer>();
+public void enqueue(int value)
+{
+    s1.push(value);
+}
+public int dequeue()
+{
+    if(s1.isEmpty() && s2.isEmpty())
+    {
+        System.out.println("queue is empty");
+         System.exit(0);
+    }
+    else if(s2.isEmpty())
+    {
+        while(!s1.isEmpty())
+        {
+            s2.push(s1.pop());
+        }
+    }
+      return s2.pop();
+}
+public static void main(String[] args)
+{
+    QueueWithStack queueWithStack=new QueueWithStack();
+    queueWithStack.enqueue(1);
+    queueWithStack.enqueue(2);
+    queueWithStack.enqueue(3);
+    System.out.println(queueWithStack.dequeue());
+    System.out.println(queueWithStack.dequeue());
+    System.out.println(queueWithStack.dequeue());
+}
+}
